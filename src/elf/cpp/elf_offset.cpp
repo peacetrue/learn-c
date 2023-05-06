ddf53541e3e0fbf06295876ab30845a6acd82a84
peacetrue <xiayouxue@gmail.com>
2023-05-06 16:33:24 +0800

    计算 elf 文件偏移

---
+++ src/elf/cpp/elf_offset.cpp
@@ -0,0 +1,22 @@
+/** 用于计算 ELF 文件内各个段的偏移。 */
+
+#include <cstdio>
+
+int main() {
+    int start_of_program_headers = 64;
+    int number_of_program_headers = 13;
+    int size_of_program_headers = 56;
+    int total_size_of_program_headers = number_of_program_headers * size_of_program_headers;
+    printf("total_size_of_program_headers: %i\n", total_size_of_program_headers);
+    printf("end_of_program_headers: 0x%x\n", start_of_program_headers + total_size_of_program_headers);
+
+    int start_of_section_headers = 13968;
+    int number_of_section_headers = 64;
+    int size_of_section_headers = 30;
+    int total_size_of_section_headers = number_of_section_headers * size_of_section_headers;
+    printf("start_of_section_headers: 0x%x\n", start_of_section_headers);
+    printf("total_size_of_section_headers: %i\n", total_size_of_section_headers);
+    printf("end_of_section_headers: 0x%x\n", start_of_section_headers + total_size_of_section_headers);
+
+    printf("temp: 0x%x\n", 0x00002fb0 + 0x0000000000000050);
+}
