9c5e767f741c52024c794cb2fbf460e72b9c68d1
peacetrue <xiayouxue@gmail.com>
2023-05-07 10:52:20 +0800

    实现一个自定义的 bash 程序，用于演示 bash 的大致逻辑。

--- src/elf/c/minibash.c
+++ src/elf/c/minibash.c
@@ -1,17 +1,17 @@
 /** 实现一个自定义的 bash 程序，用于演示 bash 的大致逻辑。 */
-#include <cstdio>
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
 int main() {
     char buf[1024] = {0};
     pid_t pid;
-    while (true) {
+    while (1) {
         printf("minibash$");
         scanf("%s", buf);
         pid = fork();
         if (pid == 0) {
-            if (execlp(buf, nullptr) < 0) {
+            if (execlp(buf, NULL) < 0) {
                 printf("exec error\n");
             }
         } else if (pid > 0) {
