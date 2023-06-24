f9441db5c33b9030cce1100ac6a151fdaa40cc44
peacetrue <xiayouxue@gmail.com>
2023-06-24 14:23:11 +0800

    虚拟内存分配

--- src/case/c/heap.c
+++ src/case/c/heap.c
@@ -13,32 +13,37 @@ long resolveArg(int argc, char **argv, int index, long defaults) {
  * @param argc 参数数目
  * @param argv 参数指针。
  *  <p/> argv[0]：程序路径
- *  <p/> argv[1]：内存分配方式，默认为 1。 1：mmap，2：brk，3：malloc
- *  <p/> argv[2]：内存分配空间，默认为 0，不分配空间
- *  <p/> argv[3]：睡眠时长（秒），默认 1 秒。程序驻留在运行中，以便观察内存映射
+ *  <p/> argv[1]：内存分配方式，默认为 1。 1：brk，2：mmap，3：malloc
+ *  <p/> argv[2]：内存分配次数，默认为 0
+ *  <p/> argv[3]：内存分配空间，默认为 0，不分配空间
+ *  <p/> argv[4]：睡眠时长（秒），默认 1 秒。程序驻留在运行中，以便观察内存映射
  *  @return 退出码
  */
 int main(int argc, char **argv) {
-    long size = resolveArg(argc, argv, 1, 0);
-    long way = resolveArg(argc, argv, 2, 1);
-    long seconds = resolveArg(argc, argv, 3, 1);
-    printf("size: %ld, way: %ld, seconds: %ld\n", size, way, seconds);
-    if (size == 0) {
+    int index = 0;
+    long way = resolveArg(argc, argv, ++index, 1);
+    long count = resolveArg(argc, argv, ++index, 0);
+    long size = resolveArg(argc, argv, ++index, 0);
+    long seconds = resolveArg(argc, argv, ++index, 1);
+    printf("way: %ld, count: %ld, size: %ld, seconds: %ld\n", way, count, size, seconds);
+    if (way <= 0 || way > 3 || count <= 0 || size <= 0) {
         if (seconds > 0) sleep(seconds);
         return 0;
     }
 
-    size *= 1024 * 1024; //内存空间 M
-    void *buffer;
-    if (way == 1) {
-        buffer = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    } else if (way == 2) {
-        buffer = sbrk((int) size);
-    } else if (way == 3) {
-        buffer = malloc(size);
-    } else {
-        exit(1);
+    size *= 1024 * 1024; //内存空间单位 M
+    void *buffer[count]; //内存分配的地址
+    printf("buffer location: ");
+    for (int i = 0; i < count; ++i) {
+        if (way == 1) {
+            buffer[i] = sbrk((int) size);
+        } else if (way == 2) {
+            buffer[i] = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        } else if (way == 3) {
+            buffer[i] = malloc(size);
+        }
+        printf("%p, ", buffer[i]);
     }
-    printf("buffer location: %p\n", buffer);
+    printf("\n");
     if (seconds > 0) sleep(seconds);
 }
