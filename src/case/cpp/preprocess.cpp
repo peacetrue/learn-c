f103139a2dba4733243e0150da9275c3759c7bcc
peacetrue <xiayouxue@gmail.com>
2023-06-27 04:16:44 +0800

    预处理器

---
+++ src/case/cpp/preprocess.cpp
@@ -0,0 +1,12 @@
+/**
+ * 分析预处理过程。
+ * antlr4 的 CPP 语法规则不支持（无法支持？做不到支持）预处理语法；代码中如果使用了宏定义，CPP 语法规则将解析出错。
+ * Create a C and C++ preprocessor using ANTLR？
+ * https://www.antlr3.org/pipermail/antlr-interest/2012-November/045755.html
+ * 没有资料，没有办法，自己动手？
+ */
+#define Main main
+
+int Main(int argc, char **argv) {
+
+}
